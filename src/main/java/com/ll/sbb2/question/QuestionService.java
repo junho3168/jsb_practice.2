a6239a9cfca589faa4498459bfca31349ddc5603
junho3168 <junho3168@naver.com>
2024-12-23 21:14:47 +0900

    2-09 ì서비스 활용하기

---
+++ src/main/java/com/ll/sbb2/question/QuestionService.java
@@ -0,0 +1,18 @@
+package com.ll.sbb2.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+}
