c4419ac916a580d848c172ecc27b79d90cefaf09
junho3168 <junho3168@naver.com>
2024-12-24 12:35:51 +0900

    2-12 답변 기능 만들기

--- src/main/java/com/ll/sbb2/question/QuestionController.java
+++ src/main/java/com/ll/sbb2/question/QuestionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
 import java.util.List;
-@RequestMapping
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 
